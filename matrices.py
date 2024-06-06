e9f131e5d886c195470deca69a1e5e4d1fe6deca
Logan Brenningmeyer <loganbrenningmeyer@gmail.com>
2024-06-05 22:53:14 -0500

    Update matrices.py

--- matrices.py
+++ matrices.py
@@ -1,26 +1,31 @@
 import numpy as np
 
 def main():
-    A = np.random.normal(size=(2,3))
-    B = np.random.normal(size=(3,4))
-    C = matrix_mult(A, B)
-    C_test = np.matmul(A, B)
+    # A = np.random.normal(size=(2,3))
+    # B = np.random.normal(size=(3,4))
+    # C = multiply(A, B)
+    # C_test = np.matmul(A, B)
 
-    print(C)
-    print(C_test)
+    # print(C)
+    # print(C_test)
 
-    print("Transpose Test\n")
-    A = np.random.randint(0, 10 + 1, size=(2,3))
-    print(A)
-    print(matrix_transpose(A))
-    print(A.T)
+    # print("Transpose Test\n")
+    # A = np.random.randint(0, 10 + 1, size=(2,3))
+    # print(A)
+    # print(transpose(A))
+    # print(A.T)
 
-    A = np.random.normal(size=(5,5))
-    print(A)
-    print(matrix_det(A))
-    print(np.linalg.det(A))
+    # A = np.arange(9).reshape((3,3))
+    # print(A)
+    # print(submatrix(A, 0, 0))
 
+    # print(det(A))
 
+    A = np.random.randint(0, 10, size=(3,3))
+
+    A_inverse = inverse(A)
+
+    print(multiply(A, A_inverse))
 
 '''
 Matrix Operations
@@ -31,7 +36,7 @@ Scalar Multiplication
 
 Given a matrix A and a scalar value k, multiply all of its elements by k
 '''
-def matrix_scalar_mult(A, k):
+def scalar_mult(A, k):
     for i in range(A.shape[0]):
         for j in range(A.shape[1]):
             A[i][j] = k * A[i][j]
@@ -44,7 +49,7 @@ Addition
 Given matrices A and B, the sum C is given by C_ij = A_ij + B_ij
 In other words, you add each respective element of the matrices
 '''
-def matrix_add(A, B):
+def add(A, B):
     # Shapes must match
     if A.shape != B.shape:
         return -1
@@ -64,7 +69,7 @@ Multiplication
 Given matrix A of size m*n and matrix B of size n*p, the product C_ij = sum(k=1 --> n) A_ik * B_kj
 A's columns must match the length of B's rows, and the element C_ij equals the sum of A's elements in the same row times B's elements in the same column
 '''
-def matrix_mult(A, B):
+def multiply(A, B):
     # A columns must equal B rows
     if A.shape[1] != B.shape[0]:
         return -1
@@ -76,7 +81,7 @@ def matrix_mult(A, B):
         for j in range(B.shape[1]):
             # Dot product of respective A row and B column
             for k in range(A.shape[1]):
-                C[i][j] += A[i][k] * matrix_transpose(B)[j][k]
+                C[i][j] += A[i][k] * transpose(B)[j][k]
             
     return C
 
@@ -85,7 +90,7 @@ Matrix transposition
 
 The transpose of a matrix A, denoted A^T, is obtained by sqapping rows with columns, A^T_ij = A_ji
 '''
-def matrix_transpose(A):
+def transpose(A):
     A_T = np.zeros(shape=(A.shape[1], A.shape[0]))
 
     # Columns become rows
@@ -95,6 +100,24 @@ def matrix_transpose(A):
 
     return A_T
 
+'''
+Returns the submatrix of A excluding row i and column j
+'''
+def submatrix(A, row, col):
+    sub_m = A.shape[0] - 1
+    sub_n = A.shape[1] - 1
+
+    sub = np.zeros(shape=(sub_m, sub_n))
+
+    count = 0
+
+    for i in [x for x in range(A.shape[0]) if x != row]:
+        for j in [x for x in range(A.shape[1]) if x != col]:
+            sub[count // sub_n][count % sub_n] = A[i][j]
+            count += 1
+
+    return sub
+
 
 '''
 Determinants
@@ -103,7 +126,7 @@ The determinant of a square matrix A, denoted det(A), is a scalar value that det
 For a 2x2 matrix A, det(A) = ad - bc
 
 '''
-def matrix_det(A):
+def determinant(A):
     # Verify that A is a square 2x2 matrix or larger
     if A.shape[0] != A.shape[1] or A.shape[0] == 1:
         return None
@@ -120,21 +143,13 @@ def matrix_det(A):
         # For each element in the first row
         for k in range(A.shape[1]): 
 
-            # Create a submatrix, 1 smaller in width/height
-            submatrix = np.zeros(shape=(A.shape[0] - 1, A.shape[1] - 1))
-
-            # Fill submatrix (elements not sharing first row or kth column)
-            count = 0
-            for i in range(1, A.shape[0]):
-                for j in [x for x in range(A.shape[1]) if x != k]:
-                    submatrix[i - 1][count % (A.shape[1] - 1)] = A[i][j]
-                    count += 1
+            sub = submatrix(A, 0, k)
 
             # Add determinants of submatrices
             if (k % 2 == 0):
-                det += A[0][k] * matrix_det(submatrix)
+                det += A[0][k] * determinant(sub)
             else:
-                det -= A[0][k] * matrix_det(submatrix)
+                det -= A[0][k] * determinant(sub)
 
         return det
 
@@ -143,15 +158,49 @@ Inverse of a matrix
 
 The inverse of a matrix A, denoted A^-1, is a matrix such that A*A^-1 = A^-1*A = I (identity matrix)
 '''
-def matrix_inverse(A):
+def inverse(A):
 
+    # Check if det(A) == 0
+    if determinant(A) == 0:
+        return None
+
+    m = A.shape[0]
+    n = A.shape[1]
+
+    # Calculate the matrix of Minors
+    minors = np.zeros(shape=(m, n))
+
+    for i in range(m):
+        for j in range(n):
+            # Find the submatrix
+            sub = submatrix(A, i, j)
+
+            minors[i][j] = determinant(sub)
+
+    # Calculate the matrix of Cofactors
+    count = 0
+
+    for i in range(m):
+        for j in range(n):
+            if count % 2 != 0:
+                minors[i][j] *= -1
+            count += 1
+
+    # Calculate the Adjugate
+    adjugate = transpose(minors)
+
+    # Divide by det(A)
+    inverse = scalar_mult(adjugate, 1/determinant(A))
+
+    return inverse
 
 '''
 Eigenvalues and Eigenvectors
 
 For a square matrix A, a scalar λ is an eigenvalue and a non-zero vector v is an eigenvector if Av = λv
 '''
-
+def eigenvalue(A):
+    return None
 '''
 Matrix decomposition
 '''
